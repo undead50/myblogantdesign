53f44840fa90c852a019466d2e381797413697d8
ayushpradhan <ayushpradhan588@gmail.com>
2023-06-30 10:15:15 +0545

    changes

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -6,7 +6,7 @@ const contentStyle = {
   lineHeight: '160px',
   textAlign: 'center',
   backgroundImage:
-    'url("https://images.pexels.com/photos/9122851/pexels-photo-9122851.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load")',
+    'url("https://images.pexels.com/photos/8038906/pexels-photo-8038906.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")',
   backgroundSize: 'cover',
   backgroundPosition: 'center',
 };
